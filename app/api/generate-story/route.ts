35aaaacf8df3e56e221b0d06e672f0248ec551f1
mylittlememorybox <Lamprinouxristina@gmail.com>
2026-04-27 17:07:01 +0300

    Update route.ts

--- app/api/generate-story/route.ts
+++ app/api/generate-story/route.ts
@@ -3,7 +3,7 @@ export async function POST(request: Request) {
     const body = await request.json();
 
     const response = await fetch(
-      "https://hook.eu1.make.com/4vspo1e7o6bmqg3q8rha4vl5o293q55",
+      "https://hook.eu1.make.com/4vspo1e7o6bmqg3q8rha4vl5o293qz55
       {
         method: "POST",
         headers: {
