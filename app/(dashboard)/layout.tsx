d3d5240cc44a21f002e05759469361aaae0940b3
mylittlememorybox <Lamprinouxristina@gmail.com>
2026-05-01 17:30:39 +0300

    Update layout.tsx

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,10 +3,9 @@ import Sidebar from "../components/Sidebar";
 export default function DashboardLayout({ children }) {
   return (
     <main style={{ display: "flex" }}>
+      <h1 style={{ color: "red", fontSize: "40px" }}>TEST LAYOUT</h1>
       <Sidebar />
-      <div style={{ flex: 1 }}>
-        {children}
-      </div>
+      <div style={{ flex: 1 }}>{children}</div>
     </main>
   );
 }
